78f12d71667df4b2b8b43ed4ce6bc281ca3a6e43
Casey Curry <casey.de.curry@gmail.com>
2017-03-05 09:48:46 -0600

    remove firefox

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,13 +45,6 @@ exports.config = {
     // 5 instances get started at a time.
     maxInstances: 5,
     //
-    browserName: "firefox"
-  }, {
-    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
-    maxInstances: 5,
-    //
     browserName: "chrome"
   }],
   //
