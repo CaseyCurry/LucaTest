eb979c5cf007a64a5929c90d609cef35512ff776
Casey Curry <casey.de.curry@gmail.com>
2017-03-04 19:42:02 -0600

    remove selenium standalone service

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,7 +45,7 @@ exports.config = {
     // 5 instances get started at a time.
     maxInstances: 5,
     //
-    browserName: "phantomjs"
+    browserName: "chrome"
   }],
   //
   // ===================
@@ -107,10 +107,7 @@ exports.config = {
   // Services take over a specific job you don"t want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don"t add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [
-    "selenium-standalone",
-    "phantomjs"
-  ],
+  services: [],
   //
   // see also: http://webdriver.io/guide/testrunner/frameworks.html
   framework: "cucumber",
