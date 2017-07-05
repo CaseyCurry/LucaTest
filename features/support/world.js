a631b6a60cf21571cb3c1519e589f8ae8f28af7c
Casey Curry <casey.de.curry@gmail.com>
2017-07-04 22:20:06 -0500

    test with firefox

--- features/support/world.js
+++ features/support/world.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const chai = require("chai");
-require("chromedriver");
+require("geckodriver");
 const webdriver = require("selenium-webdriver");
 const { defineSupportCode } = require("cucumber");
 
 function CustomWorld() {
   this.driver = new webdriver
     .Builder()
-    .forBrowser("chrome")
+    .forBrowser("firefox")
     .usingServer("http://localhost:4444/wd/hub")
     .build();
   this.expect = (value) => chai.expect(value);
