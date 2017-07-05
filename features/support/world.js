1357cb68422afa651efebec739f18b27b5c13358
Casey Curry <casey.de.curry@gmail.com>
2017-07-04 22:06:03 -0500

    enable remote selenium server

--- features/support/world.js
+++ features/support/world.js
@@ -1,14 +1,15 @@
 "use strict";
 
 const chai = require("chai");
-require("geckodriver");
+require("chromedriver");
 const webdriver = require("selenium-webdriver");
 const { defineSupportCode } = require("cucumber");
 
 function CustomWorld() {
   this.driver = new webdriver
     .Builder()
-    .forBrowser("firefox")
+    .forBrowser("chrome")
+    .usingServer("http://localhost:4444/wd/hub")
     .build();
   this.expect = (value) => chai.expect(value);
 }
